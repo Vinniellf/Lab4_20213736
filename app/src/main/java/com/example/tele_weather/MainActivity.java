0600329bad68718f8874e9f9f98e14b637c205d7
Vinniellf <a20213736@pucp.edu.pe>
2025-05-14 11:17:09 -0500

    plantilla

---
+++ app/src/main/java/com/example/tele_weather/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.tele_weather;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton bottom_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        bottom_start = findViewById(R.id.button_start);
+        bottom_start.setOnClickListener(view -> {
+            if (comprobarConexion()) {
+                startActivity(new Intent(MainActivity.this, AppActivity.class));
+            } else {
+                mostrarDialogoSinInternet();}
+        });
+
+
+    }
+    private boolean comprobarConexion() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (cm != null) {
+            Network network = cm.getActiveNetwork();
+
+            if (network != null) {
+                NetworkCapabilities capabilities = cm.getNetworkCapabilities(network);
+
+                if (capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private void mostrarDialogoSinInternet() {
+        new AlertDialog.Builder(this)
+                .setTitle("Sin conexión")
+                .setMessage("No hay conexión a Internet. Por favor, actívela.")
+                .setPositiveButton("Configuración", (dialog, which) -> {
+                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+}
